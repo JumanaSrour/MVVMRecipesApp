0717eb0841a4737a177637ae35e186bc49164d6f
JumanaSrour <joumana.sroor2000@gmail.com>
2022-01-16 21:52:12 +0200

    project setup

---
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.compose
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setContent {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text("Sample text")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Button(onClick = {}) {
+                    Text(text = "Hello!")
+                }
+            }
+        }
+    }
+}
