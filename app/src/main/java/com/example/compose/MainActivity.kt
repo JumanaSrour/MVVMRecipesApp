319e77daaa4290708d180fbe1be4eebb5130f291
JumanaSrour <joumana.sroor2000@gmail.com>
2022-01-18 12:56:26 +0200

    creating column layout

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +25,48 @@ class MainActivity : AppCompatActivity() {
 
         setContent {
             Column(
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState()).background(Color(0xFFF2F2F2)).fillMaxSize()
             ) {
-                Text("Sample text")
-                Spacer(modifier = Modifier.padding(top = 10.dp))
-                Button(onClick = {}) {
-                    Text(text = "Hello!")
+                Image(
+                    painterResource(
+                        R.drawable
+                            .happy_meal
+                    ),
+                    "",
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
+
+                )
+
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(
+                        text = "Happy meal",
+                        style = TextStyle(
+                            fontSize = 26.sp,
+                            fontFamily = FontFamily.SansSerif,
+                        )
+
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(
+                        text = "800 Calories",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            fontFamily = FontFamily.SansSerif,
+                        )
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(
+                        text = "$5.99",
+                        style = TextStyle(
+                            color = Color(0xff85bb65),
+                            fontSize = 18.sp,
+                            fontFamily = FontFamily.SansSerif,
+                        )
+                    )
                 }
             }
         }
